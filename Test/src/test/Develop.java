fdb8e55f706f2723102acf784d4a438d8135636a
sighBRG <55457027+sighBRG@users.noreply.github.com>
2019-10-31 10:48:55 +0100

    Develop.java
    Adding develop.java

---
+++ Test/src/test/Develop.java
@@ -0,0 +1,12 @@
+
+package test;
+
+
+public class Develop {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        a = b;
+        System.out.println(a);
+    }
+}
